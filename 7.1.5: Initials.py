5bcc6497ce0bef188e3d7028c6abceb322cef490
StrictBark <126030148+StrictBark@users.noreply.github.com>
2023-02-21 20:12:38 -0500

    Create 7.1.5: Initials.py

---
+++ 7.1.5: Initials.py
@@ -0,0 +1,7 @@
+# fill in the body of this function!
+
+def initials(first, last): 
+    return first[0] + "." + last[0] + "."
+
+print(initials("Tracy", "Turtle")) 
+print(initials("Peter", "Parker"))
