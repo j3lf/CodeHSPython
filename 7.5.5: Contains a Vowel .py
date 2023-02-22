21fbc2a1f732ac22cef9b42bb1e70225505bb36e
StrictBark <126030148+StrictBark@users.noreply.github.com>
2023-02-21 20:13:58 -0500

    Create 7.5.5: Contains a Vowel .py

---
+++ 7.5.5: Contains a Vowel .py
@@ -0,0 +1,5 @@
+# use `in` to determine if `word` contains any vowels!
+def contains_vowel(name):
+    return "a"in name or "e" in name or "i" in name or "o" in name or "u" in name
+
+print (contains_vowel("John Cena"))
