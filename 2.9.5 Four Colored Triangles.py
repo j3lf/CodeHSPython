896225923884ccfde61d19c25d1dcf02c2543dcc
StrictBark <126030148+StrictBark@users.noreply.github.com>
2023-02-21 19:50:55 -0500

    Create 2.9.5 Four Colored Triangles.py

---
+++ 2.9.5 Four Colored Triangles.py
@@ -0,0 +1,19 @@
+pensize(5)
+color("red")
+backward(100)
+forward(200)
+
+color("blue")
+left(120)
+forward(50)
+color("green")
+left(120)
+forward(50)
+
+for i in range(3):
+    right(120)
+    color("blue")
+    forward(50)
+    left(120)
+    color("green")
+    forward(50)
