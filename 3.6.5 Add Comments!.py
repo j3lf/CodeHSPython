1c0c88a5ea0388ad86a120575bc30f4f0ab9dd0c
StrictBark <126030148+StrictBark@users.noreply.github.com>
2023-02-21 20:05:18 -0500

    Create 3.6.5 Add Comments!.py

---
+++ 3.6.5 Add Comments!.py
@@ -0,0 +1,13 @@
+first_name = input("Enter your first name: ")
+middle_name = input("Enter your middle name: ")
+last_name = input("Enter your last name: ")
+
+#single line
+full_name = first_name + " " + middle_name + " " + last_name
+print(full_name)
+
+"""
+multi
+line
+comment
+"""
