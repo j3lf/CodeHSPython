cca10ef8e57116ab85512f80df4a4cef986d578f
StrictBark <126030148+StrictBark@users.noreply.github.com>
2023-02-21 20:16:18 -0500

    Create 8.2.7: Listed Greeting .py

---
+++ 8.2.7: Listed Greeting .py
@@ -0,0 +1,8 @@
+# fill in this function to greet the user!
+user_info = "Eva 1000 drawing"
+
+def greeting(user_info):
+    greeting = user_info.split()
+    return "Hello, " + (greeting[0]) + "! I also enjoy " + (greeting[-1]) + "!"
+
+print (greeting(user_info))
