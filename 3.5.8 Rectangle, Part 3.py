6e10ea9f43ee13efe25d2f6e6f02a661346299c8
StrictBark <126030148+StrictBark@users.noreply.github.com>
2023-02-21 20:04:45 -0500

    Create 3.5.8 Rectangle, Part 3.py

---
+++ 3.5.8 Rectangle, Part 3.py
@@ -0,0 +1,6 @@
+length = int(input("Length: "))
+width = int(input("Width: "))
+area = length * width
+perimeter = 2 * (length + width)
+print("Area: " + str(area))
+print("Perimeter: " + str(perimeter))
