338c1099175d2fac325c1c5071e037d5a9de0393
StrictBark <126030148+StrictBark@users.noreply.github.com>
2023-02-21 20:14:11 -0500

    Create 7.6.4: ENTHUSIASM! .py

---
+++ 7.6.4: ENTHUSIASM! .py
@@ -0,0 +1,8 @@
+# return the input capitalized and with an exclamation point!
+string = "codehs rocks"
+
+def add_enthusiasm(string):
+    string = string.upper()
+    return string + "!"
+
+print (add_enthusiasm(string))
